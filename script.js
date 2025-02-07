255e7bbb2fe9adcfb3fcf2e2338cf5c4e45c79c5
Олег Романов <oleg.romanov@rtlabs.ru>
2025-02-07 15:47:58 +0400

    init v2

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
+let tg = window.Telegram.WebApp;
+
 // Проверяем, что мы запущены как Web App
-if (Telegram.WebApp.ready) {
+if (!!tg?.ready) {
   const userInfo = document.getElementById('user-info');
   const sendBtn = document.getElementById('send-data-btn');
 
   // Получаем информацию о пользователе
-  const userFirstName = Telegram.WebApp.initDataUnsafe.user.first_name;
-  const userLastName = Telegram.WebApp.initDataUnsafe.user.last_name;
+  const userFirstName = tg.initDataUnsafe.user.first_name;
+  const userLastName = tg.initDataUnsafe.user.last_name;
 
   userInfo.textContent = `Привет, ${userFirstName} ${userLastName}!`;
 
   // Обработка нажатия кнопки
   sendBtn.addEventListener('click', () => {
     const data = 'Данные от Web App';
-    Telegram.WebApp.sendData(data); // Отправляем данные обратно в бота
-    Telegram.WebApp.close(); // Закрываем Web App
+    tg.sendData(data); // Отправляем данные обратно в бота
+    tg.close(); // Закрываем Web App
   });
 }
